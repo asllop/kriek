3a07bdec7be6ba9d0cbb570c0865908d451a8c11
Andreu Santaren Llop <andreu.santaren@gmail.com>
2022-02-19 16:10:36 +0100

    Implement hierarchical stack.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ const NAME_SIZE: usize = 32;
 type WordName = [u8; NAME_SIZE];
 
 /// Terminal Input Buffer
-struct TIB<T: Iterator<Item=u8> + Sized> {
+pub struct TIB<T: Iterator<Item=u8> + Sized> {
     reader: T
 }
 
@@ -18,7 +18,7 @@ impl<T: Iterator<Item=u8> + Sized> TIB<T> {
         let mut i = 0;
         let mut word_found = false;
         while let Some(b) = self.reader.next() {
-            // Found a word separator (comma or control character)
+            // Found a word separator (comma, space or any control character)
             if b == 44 || b <= 32 {
                 if word_found {
                     break;
@@ -36,6 +36,76 @@ impl<T: Iterator<Item=u8> + Sized> TIB<T> {
         }
         (word_name, i as u8)
     } 
+}
+
+type KrkInt = i64;
+type KrkFlt = f64;
+
+#[derive(Debug)]
+enum Cell {
+    Integer(KrkInt),
+    Float(KrkFlt),
+    WordRef(usize),
+    AllocRef(usize),
+}
+
+/// Stack structure
+struct Stack {
+    stack: Vec<Cell>,
+    base: usize,
+    nested: Vec<usize>,
+}
+
+impl Stack {
+    pub fn new() -> Self {
+        Self {
+            stack: Vec::new(),
+            base: 0,
+            nested: Vec::new(),
+        }
+    }
+
+    /// Starts a new nested stack
+    pub fn start_stack(&mut self) {
+        self.nested.push(self.base);
+        self.base = self.stack.len();
+    }
+
+    /// Ends current stack
+    pub fn end_stack(&mut self) -> Option<usize> {
+        if let Some(base) = self.nested.pop() {
+            self.base = base;
+            Some(base)
+        }
+        else {
+            None
+        }
+    }
+
+    /// Empty current stack
+    pub fn empty(&mut self) {
+        self.stack.truncate(self.base);
+    }
+
+    /// Push cell to current stack
+    pub fn push(&mut self, cell: Cell) {
+        self.stack.push(cell);
+    }
+
+    /// Pop cell from current stack
+    pub fn pop(&mut self) -> Option<Cell> {
+        if self.stack.len() > self.base {
+            self.stack.pop()
+        }
+        else {
+            None
+        }
+    }
+
+    /// Size of current stack
+    pub fn size(&self) -> usize {
+        self.stack.len() - self.base
+    }
 }
 
 /// String new type
@@ -83,4 +153,28 @@ fn main() {
             break;
         }
     }
+
+    let mut stack = Stack::new();
+    println!("Push Int 1");
+    stack.push(Cell::Integer(1));
+    println!("Push Int 2");
+    stack.push(Cell::Integer(2));
+    println!("Start new stack");
+    stack.start_stack();
+    println!("Size = {}", stack.size());
+    println!("Push Int 10");
+    stack.push(Cell::Integer(10));
+    println!("Push Int 20");
+    stack.push(Cell::Integer(20));
+    println!("Size = {}", stack.size());
+    println!("Empty current stack");
+    stack.empty();
+    println!("Size = {}", stack.size());
+    println!("Pop = {:?}", stack.pop());
+    println!("End stack");
+    stack.end_stack();
+    println!("Size = {}", stack.size());
+    println!("Pop = {:?}", stack.pop());
+    println!("Pop = {:?}", stack.pop());
+    println!("Pop = {:?}", stack.pop());
 }
