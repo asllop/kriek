f61d65f2445cfb9210fb0fd097bef6569ce4bcac
Andreu Santaren Llop <andreu.santaren@gmail.com>
2022-02-19 13:11:25 +0100

    Model the TIB.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Kriek
 _Lexicon Oriented Programming Language_
 
-## Introduction
+## Preface
 
 The Forth programming language is probably one of the most idiosyncratic and powerful Memes in computing history. When it appeared in the 70’s it was revolutionary, and all the underlying concepts still remain valuable and contemporary, but some of its details may look a bit aged if we inspect it with the optics of present state-of-the-art computer programming. A handful of nice projects appeared over the years, Factor being the most prominent, following the path traced by Forth and adding modern features. These languages include things like classes, functional operators, garbage collection, complex data primitives, etc. But in my opinion, they lose part of the essence of Forth. In contrast, **Kriek** pursues simplicity, and instead of borrowing concepts from other programming philosophies, it rests upon the set of tools and ideas that originally backed Forth: It extends and delves into the notion of **Lexicon**, which proved to be powerful, compact, and flexible. For this reason, I call Kriek a **Lexicon Oriented Programming Language**.
 
@@ -12,3 +12,7 @@ Kriek received influences from all the programming languages I ever used (and li
 - Rust: traits inspired lexicon unions.
 - Racket: lists inspired nested stacks.
 - Swift: automatic reference counting.
+
+## Introduction
+
+Kriek is a [stack-based](https://en.wikipedia.org/wiki/Stack-oriented_programming), [concatenative](https://en.wikipedia.org/wiki/Concatenative_programming_language)  programming language, designed to be easily embedded into other applications.
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,86 @@
+const NAME_SIZE: usize = 32;
+type WordName = [u8; NAME_SIZE];
+
+/// Terminal Input Buffer
+struct TIB<T: Iterator<Item=u8> + Sized> {
+    reader: T
+}
+
+impl<T: Iterator<Item=u8> + Sized> TIB<T> {
+    pub fn new(reader: T) -> Self {
+        Self {
+            reader
+        }
+    }
+
+    pub fn next_word(&mut self) -> (WordName, u8) {
+        let mut word_name = WordName::default();
+        let mut i = 0;
+        let mut word_found = false;
+        while let Some(b) = self.reader.next() {
+            // Found a word separator (comma or control character)
+            if b == 44 || b <= 32 {
+                if word_found {
+                    break;
+                }
+            }
+            else {
+                word_found = true;
+                word_name[i] = b;
+                i += 1;
+            }
+
+            if i >= NAME_SIZE {
+                break;
+            }
+        }
+        (word_name, i as u8)
+    } 
+}
+
+/// String new type
+struct StringWrap {
+    string: String,
+    index: usize,
+}
+
+impl StringWrap {
+    pub fn new(string: &str) -> Self {
+        Self {
+            string: String::from(string),
+            index: 0
+        }
+    }
+}
+
+impl Iterator for StringWrap {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let bytes = self.string.as_bytes();
+        if bytes.len() > self.index {
+            let b = bytes[self.index];
+            self.index += 1;
+            Some(b)
+        }
+        else {
+            None
+        }
+    }
+}
+
 fn main() {
+    let program = StringWrap::new("is this a   \n program,with , many\twords?");
+    let mut tib = TIB::new(program);
+    
+    loop {
+        let (next_word, word_len) = tib.next_word();
+        if word_len > 0 {
+            let word_name = std::str::from_utf8(&next_word).unwrap_or_default();
+            println!("Word read = {}", word_name)
+        }
+        else {
+            break;
+        }
+    }
 }
